fe5287a0f5346c91274a98b819c26282571023da
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-03-17 09:09:29 +0100

    Update platforms and add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 // TODO
-// Docstrings
-// Maybe refactor so it can be tested? Make tests work
-
-// Other things
 // src/concatenate and src/create fasta should not need to load vamb
 
 // Requires a FASTA parser in Rust, w. sequence validation and
@@ -15,6 +11,10 @@ use std::borrow::Cow;
 use numpy::{PyReadwriteArray1, PyReadwriteArray2};
 use pyo3::prelude::*;
 
+// Make lookup table for ACGT -> 0123.
+// Ambiguous nucleotides are 4.
+// Invalid nucleotides are assumed not to exist, as they should have been
+// checked for on the Python side.
 const fn make_lut() -> [u8; 256] {
     let mut lut = [4u8; 256];
     let mut i: usize = 0;
@@ -23,7 +23,7 @@ const fn make_lut() -> [u8; 256] {
             b'A' | b'a' => lut[i] = 0,
             b'C' | b'c' => lut[i] = 1,
             b'G' | b'g' => lut[i] = 2,
-            b'T' | b't' => lut[i] = 3,
+            b'T' | b't' | b'U' | b'u' => lut[i] = 3,
             _ => (),
         };
         i += 1;
@@ -33,6 +33,8 @@ const fn make_lut() -> [u8; 256] {
 
 const LUT: [u8; 256] = make_lut();
 
+/// Count fourmers in a byte array. Takes a NumPy array of u32 counts, and a byte array.
+/// The bytes must only contain IUPAC nucleotides. The counts must be zeroed, and have length 256.
 #[pyfunction]
 fn kmercounts(mut counts: PyReadwriteArray1<u32>, bytes: Cow<[u8]>) {
     let mut kmer = 0u32;
@@ -54,7 +56,8 @@ fn kmercounts(mut counts: PyReadwriteArray1<u32>, bytes: Cow<[u8]>) {
         kmer = ((kmer << 2) | (val as u32)) & mask;
 
         if countdown == 0 {
-            // Safety: We just masked the lower 2k bits, so kmer is in the range 0..2^2k
+            // Safety: We just masked the lower 8 bits, so kmer is in the range 0..=255.
+            // Since we checked before the loop that counts has length 256, this must be inbounds.
             unsafe {
                 *counts_slice.get_unchecked_mut(kmer as usize) += 1;
             }
@@ -64,6 +67,8 @@ fn kmercounts(mut counts: PyReadwriteArray1<u32>, bytes: Cow<[u8]>) {
     }
 }
 
+// Given a slice of bool values, iterates over Range<usize> where the bools are true,
+// in order.
 struct ContiguousTrueRanges<'a> {
     mask: &'a [bool],
     pos: usize,
@@ -75,6 +80,8 @@ impl<'a> ContiguousTrueRanges<'a> {
     }
 }
 
+// TODO: We could make this faster by using the bytemuck crate to cast the bools to a &[u8],
+// then use memchr.
 impl Iterator for ContiguousTrueRanges<'_> {
     type Item = std::ops::Range<usize>;
 
@@ -113,7 +120,6 @@ impl Iterator for ContiguousTrueRanges<'_> {
     }
 }
 
-// Add a test for ContiguousTrueRanges
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -142,6 +148,10 @@ mod tests {
     }
 }
 
+/// Given matrix, a 2D NumPy array of float32, and a mask, a 1D NumPy array of bool, where
+/// the length of mask is equal to the number of rows in matrix, this function works like
+/// matrix[mask], except in-place.
+/// It returns the number of filled rows. The matrix must then be truncated on the Python side.
 #[pyfunction]
 fn overwrite_matrix(mut matrix: PyReadwriteArray2<f32>, mask: PyReadwriteArray1<bool>) -> usize {
     let nrow = matrix.as_array().shape()[0];
@@ -159,7 +169,6 @@ fn overwrite_matrix(mut matrix: PyReadwriteArray2<f32>, mask: PyReadwriteArray1<
     for range in ContiguousTrueRanges::new(mask.as_slice().unwrap()) {
         // If range.start is 0, then it copies into its own position needlessly
         if range.start != 0 {
-            // Range here is all wrong
             matrix.copy_within(range.start * ncol..range.end * ncol, write_row_index * ncol);
         }
         write_row_index += range.len();
@@ -168,7 +177,7 @@ fn overwrite_matrix(mut matrix: PyReadwriteArray2<f32>, mask: PyReadwriteArray1<
     write_row_index
 }
 
-/// A Python module implemented in Rust.
+/// Low-level functionality of Vamb which can't easily be done in Python
 #[pymodule]
 fn vambcore(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(kmercounts, m)?)?;
